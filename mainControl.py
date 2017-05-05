e98758f0539000239a888ef139b6db457daf8e11
agomezz26 <agomez26@binghamton.edu>
2017-05-04 23:53:30 -0400

    Add files via upload

---
+++ mainControl.py
@@ -0,0 +1,121 @@
+import tkinter
+from tkinter import *
+import question
+'''import csv
+import json'''
+
+
+master = Tk()
+master.title("Roommate Match")
+#master.geometry(("900X900"))
+#master.wm_iconbitmap("Troll_Face.ico")
+
+class Questionaire(Frame):	
+    def createEntry(self,question):
+        Label(master,text=question).grid(row= self.row_count)
+        e = Entry(master)
+        e.grid(row=self.row_count+1,column=1)
+        self.row_count+=2
+        
+
+    def create_yesno(self, question):
+        var= IntVar()
+        column=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="yes", variable= var, value= 1).grid(row = self.row_count,column = column, sticky = W)
+        column+=1
+        Radiobutton(master, text="no", variable= var, value= 2).grid(row = self.row_count,column = column, sticky = W)
+        self.answer_key[question] = var        
+        self.row_count+=1
+        
+
+    def multi_choice(self, question):
+        var= IntVar()
+        column=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="frequently", variable= var, value= 1).grid(row = self.row_count,column = column, sticky = W)
+        column+=1
+        Radiobutton(master, text="moderate", variable= var, value= 2).grid(row = self.row_count,column = column, sticky = W)
+        column+=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="never", variable= var, value= 3).grid(row = self.row_count,column = column+2)
+        column+=1        
+        self.row_count+=1
+        
+
+    def multi_answr(self, question):
+        var= IntVar()
+        column=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="before 10", variable= var, value= 1).grid(row = self.row_count,column = 1, sticky = W)
+        column+=1
+        Radiobutton(master, text="10-11", variable= var, value= 2).grid(row = self.row_count,column = 2, sticky = W)
+        self.answer_key[question] = var        
+        column=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="11-12", variable= var, value= 3).grid(row = self.row_count,column = 3, sticky = W)
+        column+=1
+        Radiobutton(master, text="after 12", variable= var, value= 4).grid(row = self.row_count,column = 4, sticky = W)
+        column+=1
+        self.row_count+=1
+        self.answer_key[question] = var
+        
+    '''def callback():
+        peeps = {}
+        done = 'n'
+        
+        while done == 'n':
+            peeps.append(name)
+        peeps_str = json.dumps(peeps)
+        fptr = open('people.json', 'w')
+        fptr.write(peeps_str)
+        fptr.close()'''
+        
+    def money(self,question):
+        var= IntVar()
+        column=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="200-400", variable= var, value= 1).grid(row = self.row_count,column = 1, sticky = W)
+        column+=1
+        Radiobutton(master, text="400-600", variable= var, value= 2).grid(row = self.row_count,column = 2, sticky = W)
+        column+=1
+        Label(master, text = question).grid(row = self.row_count)
+        Radiobutton(master, text="600-800", variable= var, value= 3).grid(row = self.row_count,column = 3, sticky = W)
+        column+=1
+        Radiobutton(master, text="800+", variable= var, value= 4).grid(row = self.row_count,column = 4, sticky = W)
+        self.answer_key[question] = var
+
+ #   def createWidgets(self):
+ #       Button(master, text= "Quit", command=master.quit).grid(row= self.row_count, sticky=W, pady=4)
+ #       Button(master, text= "Submit", command= Questionaire.callback()).grid(row= self.row_count, column=2)
+
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.row_count=5
+ #       self.createWidgets()
+        self.answer_key = {}
+
+q_sheet = Questionaire()
+
+for i in question.fill_in().fq:
+    q_sheet.createEntry(i)
+for i in question.ynquestions().qs:
+    q_sheet.create_yesno(i)
+for i in question.multquestions().mq:
+    q_sheet.multi_choice(i)
+for i in question.mult_answer().mas:
+    q_sheet.multi_answr(i)
+for i in question.rent_q().mo:
+    q_sheet.money(i)
+
+
+'''csvfile = open('.csv', 'r')
+jsonfile = open('people.json', 'w')
+fieldnames = ("FirstName","LastName","IDNumber","Message")
+reader = csv.DictReader( csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')'''
+       
+master.mainloop()
---
+++ question.py
@@ -0,0 +1,64 @@
+class ynquestions:    
+	def __init__(self):
+		self.qs=["Do you smoke?", "Do you consider yourself a light sleeper?","Do you have and/or want pets?", "Do you mind if your roommate has frequent overnight guests?", "Are you willing to split the grocery bill?", "Are you willing to split chores in a shared living space?","Do you have any dietary restrictions, be it from allergies or personal choice? ", "Do you mind if your roommate has frequent daytime guests if they leave at a time you find appropriate?",]
+
+class fill_in:
+	def __init__(self):
+		self.fq= ["Full Name: ","Gender: ","What state are you from?(Please enter state abbreviation): ", "Age:", "Leave a personal message or a short description of yourself: "]
+
+class multquestions:
+	def __init__(self):
+		self.mq= ["How often do you workout?", "How often do you clean?", "How often do you go out?", "How often are you willing to commute?"]
+
+class mult_answer:
+        def __init__(self):
+            self.mas=["At about what hour do you go to sleep?", "At about what hour do you wake up?"]
+
+class rent_q:
+        def __init__(self):
+            self.mo=["How much are you looking to spend on rent, including utilities, per month?"]
+
+
+        '''def num_match(self, match):
+            num=0
+                if(self.qs[1]== match.q1):
+                    num +=25
+                if(self.q2== match.q2):
+                    num +=1
+                if(self.q3== match.q3):
+                    num +=5
+                if(self.q4== match.q4):
+                    num +=5
+                if(self.q5== match.q5):
+                    num +=1
+                if(self.q6== match.q6):
+                    num +=1
+                if(self.q7== match.q7):
+                    num +=1
+                if(self.q8== match.q8):
+                    num +=3
+                if(self.q9== match.q9):
+                    num +=1
+                if(self.q10== match.q10):
+                    num +=1
+                if(self.q11== match.q11):
+                    num +=3
+                if(self.q12== match.q12):
+                    num +=25
+                if(self.q13== match.q13):
+                    num +=3
+                if(self.q14== match.q14):
+                    num +=1
+                if(self.q15== match.q15):
+                    num +=25
+
+            return num + "% Match"'''
+
+
+
+
+
+
+
+
+
